b83dadc9ef052b6069a39eb48a0cba763b251ff6
Misha <misha.dorozhkin@mail.ru>
2023-07-31 19:18:09 +0300

    Добаввил файл с тестами

--- tests.py
+++ tests.py
@@ -4,10 +4,17 @@ import utils
 def test_load_operation():
     assert type(utils.load_operaion()) == list
 
+
 def test_sort_operations():
     assert type(utils.sort_operations()) == list
     for operation in utils.sort_operations():
         assert operation != {}
+    assert type(operation["date"]) == str
+    assert utils.load_operaion() != utils.sort_operations()
+    assert utils.sort_operations()[0]["date"] > utils.sort_operations()[1]["date"]
+    assert utils.sort_operations()[2]["date"] > utils.sort_operations()[3]["date"]
+
+
 
 def test_get_five():
     assert type(utils.get_five()) == list
@@ -15,6 +22,10 @@ def test_get_five():
     five = utils.get_five()
     for data in five:
         assert data["state"] == "EXECUTED"
+        assert data != {}
+    assert type(data["date"]) == str
+    assert utils.get_five()[0]["date"] > utils.get_five()[1]["date"]
+    assert utils.get_five()[2]["date"] > utils.get_five()[3]["date"]
 
 def test_mask():
     assert utils.mask("Visa Platinum 1813166339376336") == "Visa Platinum 1813 16** **** 6336"
