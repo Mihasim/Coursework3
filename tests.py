4c40d5559a8b5699a0afd67739e934a68a91b9aa
Misha <misha.dorozhkin@mail.ru>
2023-07-31 19:33:20 +0300

    Добаввил файл с тестами

--- tests.py
+++ tests.py
@@ -33,12 +33,12 @@ def test_mask():
     assert utils.mask("Visa Platinum 1813166339376336") == "Visa Platinum 1813 16** **** 6336"
     assert utils.mask("Maestro 1913883747791351") == "Maestro 1913 88** **** 1351"
     with pytest.raises(TypeError):
-        utils.mask() == ""
+        utils.mask()
 
 def test_mask_to():
     assert utils.mask_to("Visa Platinum 1813166339376336") == "Visa Platinum **6336"
     assert utils.mask_to("Maestro 1913883747791351") == "Maestro **1351"
     with pytest.raises(TypeError):
-        utils.mask_to() == ""
+        utils.mask_to()
 
 
